2fe1345f2bc91b4859dec47d89f8e80459e8b2ed
jumphoto <jumanahphotography@gmail.com>
2020-11-18 00:02:40 +0200

    Intial user routes include error message and connection to DB for search and add user

---
+++ routes/users.js
@@ -0,0 +1,43 @@
+const { request } = require('express')
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+
+//all users/authors route 
+router.get('/', async (req, res,) => {
+    try {
+        const users = await User.find({})
+        res.render('users/index'),{users}
+    } catch (error) {
+        res.redirect('/')
+    }
+    
+})
+
+
+//new users route
+router.get('/new', (req, res,) => {
+    res.render('users/new', {user: new User()})
+})
+
+//create user route
+router.post('/', async (req, res,) => {
+    const user = new User({
+        name: req.body.name
+    })
+    try {
+       const newUser = await user.save
+        //res.redirect(`users/${newUser.id}`)
+        res.redirect(`users`)
+    } catch (error) {
+        res.render('users/new', {
+            user: user,
+            errorMessage: 'Error creating user'
+        })
+
+    }
+
+})
+
+
+module.exports = router
